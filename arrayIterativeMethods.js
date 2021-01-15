2219341da7ba675d2566edbf2d2d6c0884f31b2c
soyalextreme <soyalextreme@gmail.com>
2021-01-15 03:40:03 -0600

    [add] .find .forEach .some

--- arrayIterativeMethods.js
+++ arrayIterativeMethods.js
@@ -14,13 +14,30 @@ function main(){
 
     let articlesFilter = articles.filter((article)=>{
         return article.costo <= 500;
-    })
+    });
     console.log(articlesFilter);
 
     let nameArticles = articles.map((article)=>{
         return article.nombre;
-    })
+    });
     console.log(nameArticles);
+
+    let findedArticles = articles.find((article)=>{
+        return article.nombre === "laptop";
+    });
+    console.log(findedArticles);
+
+    articles.forEach((article, index)=>{
+        console.log(`${article.nombre} | ${index}`);
+    });
+
+    let cheapArticles = articles.some((article)=>{
+        // regresa solamente verdadero o falso si la condicion se cumple.
+        return article.costo <= 700;
+    });
+
+    console.log(cheapArticles);
+
 
 }
 
